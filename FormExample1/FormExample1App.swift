8c820e82646a68a7d07ac32af712e3c91d29835a
Pierre Simon <pierre.simon.pro87@gmail.com>
2024-05-06 12:11:57 +0200

    Initial Commit

---
+++ FormExample1/FormExample1App.swift
@@ -0,0 +1,17 @@
+//
+//  FormExample1App.swift
+//  FormExample1
+//
+//  Created by Pierre Simon on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FormExample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
