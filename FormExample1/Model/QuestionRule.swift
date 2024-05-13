8216b3a98eb12a48eed609c9fc5d343511142153
Pierre Simon <pierre.simon.pro87@gmail.com>
2024-05-13 16:51:40 +0200

    simplifying rule evaluation

--- FormExample1/Model/QuestionRule.swift
+++ FormExample1/Model/QuestionRule.swift
@@ -13,30 +13,20 @@ enum QuestionRule {
 }
 
 extension QuestionRule {
-    func error(for answer: Answer) -> String? {
+    func error(for answer: Answer) -> QuestionError {
         switch self {
         case .regex(let regex, let errorMessage):
-            if answer.isEmpty { return nil }
-            return answer.matches(regex) ? nil : errorMessage
+            if answer.isEmpty { return .noError }
+            return answer.matches(regex) ? .noError : .questionError(errorMessage: errorMessage)
         case .isCompulsory(let errorMessage):
-            return answer.isEmpty ? errorMessage : nil
+            return answer.isEmpty ? .questionError(errorMessage: errorMessage) : .noError
         }
     }
 }
 
 extension Array where Element == QuestionRule {
     func error(for answer: Answer) -> QuestionError {
-        return self.reduce(QuestionError.noError) { currentError, rule in
-            switch currentError {
-            case .noError:
-                guard let errorMessage = rule.error(for: answer) else {
-                    return .noError
-                }
-                return  .questionError(
-                    errorMessage: errorMessage
-                )
-            case .questionError: return currentError
-            }
-        }
+        let firstBrokenRule = self.first { $0.error(for: answer) != .noError }
+        return firstBrokenRule?.error(for: answer) ?? .noError
     }
 }
